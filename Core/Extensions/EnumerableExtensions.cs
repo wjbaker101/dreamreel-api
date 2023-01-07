05f0dc6010c3c8b31b8c59234f5ea70eaa6f81e1
wjbaker101 <willbaker10198@gmail.com>
2023-01-07 13:48:11 +0000

    Create extensions method for converting enumerables to lists

---
+++ Core/Extensions/EnumerableExtensions.cs
@@ -0,0 +1,14 @@
+﻿namespace Core.Extensions;
+
+public static class EnumerableExtensions
+{
+    public static List<TResult> ConvertAll<T, TResult>(this IEnumerable<T> enumerable, Func<T, TResult> func)
+    {
+        return enumerable.Select(func).ToList();
+    }
+
+    public static List<TResult> ConvertAll<T, TResult>(this IEnumerable<T> enumerable, Func<T, int, TResult> func)
+    {
+        return enumerable.Select(func).ToList();
+    }
+}
